21965e361e1396961145432f7a9ce4c612a7228c
Alex Pirciu <alex.pirciu@zohomail.eu>
2020-11-21 01:27:48 +0200

    Add logic for retrieving data from the api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { useState, useEffect } from "react";
 import Map from "./components/Map";
 
 function App() {
-  return (
-    <div>
-      <Map />
-    </div>
-  );
+    const [eventData, setEventData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            setLoading(true);
+            const res = await fetch(
+                "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?api_key=WJZh3ZjsvDbpSUpWHIp1PtTe2Sb696phh6aHboV4"
+            );
+            const { events } = await res.json();
+
+            setEventData(events);
+            setLoading(false);
+        };
+
+        fetchEvents();
+
+        console.log(eventData);
+    }, []);
+
+    return (
+        <div>
+            <Map />
+        </div>
+    );
 }
 
 export default App;
